b18f1d60f93d23f7f3f499e4f6bca569e7ffa44e
Paul Tagliamonte <tag@pault.ag>
2013-12-15 17:59:27 -0500

    add bootstrap nonsense

--- metadata/views.py
+++ metadata/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
+from metadata.models import Meta
 
 #  /                     => home
 #  /metas                => metas
 #  /meta/slug            => meta
 #  /puzzle/slug          => puzzle
-#  /edit                 => edit
-#  /edit/meta/slug       => edit meta
-#  /edit/puzzle/slug     => edit puzzle
+#  /edit/meta/slug       => metaedit
+#  /edit/puzzle/slug     => puzzleedit
 
 
 def home(request):
-    return render(request, 'metadata/index.html', {},
-                  content_type="text/html")
+    return render(request, 'metadata/index.html', {
+    }, content_type="text/html")
+
+
+def metas(request):
+    return render(request, 'metadata/metas.html', {
+        "metas": Meta.objects.all()
+    }, content_type="text/html")
