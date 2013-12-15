9c311a6118cf521a01042ec7c5eacfc634d9f4ca
Paul Tagliamonte <tag@pault.ag>
2013-12-15 16:30:36 -0500

    include licensing

--- metadata/models.py
+++ metadata/models.py
@@ -1,3 +1,47 @@
+# Copyright (c) 2013, Paul R. Tagliamonte <tag@pault.ag>
+# 
+# This file is part of metadata.
+# 
+# metadata is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 from django.db import models
 
-# Create your models here.
+
+class Meta(models.Model):
+    pass
+    # answer
+    # author
+    #
+
+class MetaDocument(models.Model):
+    pass
+    # link
+    # link_type
+    # author
+    #
+
+
+class Puzzle(models.Model):
+    pass
+    # author
+    # title
+    #
+
+
+class PuzzleDocument(models.Model):
+    pass
+    # link
+    # link_type
+    # author
+    #
